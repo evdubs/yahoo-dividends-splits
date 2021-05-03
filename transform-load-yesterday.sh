0a10041c61fbadbef4480d4b16685516d969b07f
evdubs <1124030+evdubs@users.noreply.github.com>
2021-05-02 19:54:56 -1000

    add dump dolt for dividends

--- transform-load-yesterday.sh
+++ transform-load-yesterday.sh
@@ -6,3 +6,5 @@ dir=$(dirname "$0")
 racket ${dir}/transform-load.rkt -d ${yesterday} -p "$1"
 
 7zr a /var/tmp/yahoo/dividends-splits/${yesterday}.7z /var/tmp/yahoo/dividends-splits/${yesterday}/*.csv
+
+racket ${dir}/dump-dolt-dividends.rkt -p "$1"
