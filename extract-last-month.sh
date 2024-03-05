b3dd0cce01d5eaa6db1eafb06ef75935bd534b93
evdubs <1124030+evdubs@users.noreply.github.com>
2024-03-05 12:54:43 -1000

    retrieve last 3 months instead of 1 month to make sure any outages have extra attempts at coverage

--- extract-last-month.sh
+++ extract-last-month.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-month_ago=$(date -d "-1 month" "+%F")
+month_ago=$(date -d "-3 month" "+%F")
 dir=$(dirname "$0")
 
 racket -y ${dir}/extract.rkt -c "$1" -p "$2" -r "$3" -s ${month_ago}
